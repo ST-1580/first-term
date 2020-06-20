96189b116b4958b69275eb331ac12ac50acc5bdd
Yaroslav <steep1580@yandex.ru>
2020-06-20 23:54:34 +0300

    First commit vector

--- vector/vector.h
+++ vector/vector.h
@@ -1,3 +1,7 @@
+#include <cstdio>
+#include <algorithm>
+#include <assert.h>
+
 template <typename T>
 struct vector
 {
@@ -9,6 +13,7 @@ struct vector
     vector& operator=(vector const& other); // O(N) strong
 
     ~vector();                              // O(N) nothrow
+
 
     T& operator[](size_t i);                // O(1) nothrow
     T const& operator[](size_t i) const;    // O(1) nothrow
@@ -35,28 +40,275 @@ struct vector
 
     void swap(vector&);                     // O(1) nothrow
 
-    iterator begin();                       // O(1) nothrow
-    iterator end();                         // O(1) nothrow
+    iterator begin() {
+        return data_;
+    }
 
-    const_iterator begin() const;           // O(1) nothrow
-    const_iterator end() const;             // O(1) nothrow
+    iterator end() {
+        return data_ + size_;
+    }
 
-    iterator insert(iterator pos, T const&); // O(N) weak
+    const_iterator begin() const {
+        return data_;
+    }
+
+    const_iterator end() const {
+        return data_ + size_;
+    }
+
     iterator insert(const_iterator pos, T const&); // O(N) weak
 
-    iterator erase(iterator pos);           // O(N) weak
     iterator erase(const_iterator pos);     // O(N) weak
 
-    iterator erase(iterator first, iterator last); // O(N) weak
     iterator erase(const_iterator first, const_iterator last); // O(N) weak
 
 private:
-    size_t increase_capacity() const;
     void push_back_realloc(T const&);
-    void new_buffer(size_t new_capacity);
+    void new_buffer(std::size_t new_capacity);
 
 private:
     T* data_;
     size_t size_;
     size_t capacity_;
 };
+
+template<typename T>
+vector<T>::vector() {
+    size_ = 0;
+    capacity_ = 0;
+    data_ = nullptr;
+}
+
+template<typename T>
+vector<T>::vector(vector const &other) : vector() {
+    T* new_data = nullptr;
+    if (other.size_ > 0 ) {
+        new_data = static_cast<T*>(operator new(other.size_ * sizeof(T)));
+    }
+    for (size_t i = 0; i < other.size_; i++) {
+        new(new_data + i) T(other.data_[i]);
+    }
+    data_ = new_data;
+    size_ = other.size_;
+    capacity_ = other.size_;
+}
+
+template<typename T>
+vector<T> &vector<T>::operator=(vector const &other) {
+    vector<T> new_vector (other);
+    swap(new_vector);
+    return *this;
+}
+
+template<typename T>
+vector<T>::~vector() {
+    for (size_t i = 0; i < size_; i++) {
+        data_[i].~T();
+    }
+    operator delete(data_);
+}
+
+template<typename T>
+T &vector<T>::operator[](size_t i) {
+    assert(i < size_);
+    return data_[i];
+}
+
+template<typename T>
+T const &vector<T>::operator[](size_t i) const {
+    assert(i < size_);
+    return data_[i];
+}
+
+template<typename T>
+T *vector<T>::data() {
+    return data_;
+}
+
+template<typename T>
+T const *vector<T>::data() const {
+    return data_;
+}
+
+template<typename T>
+size_t vector<T>::size() const {
+    return size_;
+}
+
+template<typename T>
+T &vector<T>::front() {
+    return data_[0];
+}
+
+template<typename T>
+T const &vector<T>::front() const {
+    return data_[0];
+}
+
+template<typename T>
+T &vector<T>::back() {
+    return data_[size_ - 1];
+}
+
+template<typename T>
+T const &vector<T>::back() const {
+    return data_[size_ - 1];
+}
+
+template<typename T>
+void vector<T>::push_back(const T &value) {
+    if (size_ < capacity_) {
+        new (data_ + size_) T(value);
+    } else {
+        push_back_realloc(value);
+    }
+    size_++;
+}
+
+template<typename T>
+void vector<T>::pop_back() {
+    assert(size_ != 0);
+    data_[size_ - 1].~T();
+    size_--;
+}
+
+template<typename T>
+bool vector<T>::empty() const {
+    return size_ == 0;
+}
+
+template<typename T>
+size_t vector<T>::capacity() const {
+    return capacity_;
+}
+
+template<typename T>
+void vector<T>::reserve(size_t len) {
+    if (len > capacity_) {
+        T* new_data = static_cast<T*>(operator new(len * sizeof(T)));
+        size_t i;
+        try {
+            for (i = 0; i < size_; i++) {
+                new(new_data + i) T(data_[i]);
+            }
+        } catch (...) {
+            for (size_t j = 0; j < i; j++) {
+                new_data[j].~T();
+            }
+            throw;
+        }
+        this->~vector();
+        data_ = new_data;
+        capacity_ = len;
+    }
+}
+
+template<typename T>
+void vector<T>::shrink_to_fit() {
+    if (size_ == 0) {
+        vector <T> empty;
+        swap(empty);
+    } else if (size_ < capacity_) {
+        T *new_data = static_cast<T*>(operator new(size_ * sizeof(T)));
+        for (size_t i = 0; i < size_; i++) {
+            new(new_data + i) T(data_[i]);
+        }
+        for (size_t i = 0; i < size_; i++) {
+            data_[i].~T();
+        }
+        operator delete(data_);
+        data_ = new_data;
+    }
+    capacity_ = size_;
+}
+
+template<typename T>
+void vector<T>::clear() {
+    for (size_t i = 0; i < size_; i++) {
+        data_[i].~T();
+    }
+    size_ = 0;
+}
+
+template<typename T>
+void vector<T>::swap(vector & other) {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+}
+
+template<typename T>
+typename vector<T>::iterator vector<T>::insert(vector::const_iterator pos, const T &val) {
+    size_t id = pos - data_;
+    size_t new_capacity = capacity_;
+    if (capacity_ == 0) {
+        new_capacity = 1;
+    } else if (capacity_ == size_) {
+        new_capacity = 2 * capacity_;
+    }
+    new_buffer(new_capacity);
+    new(data_ + size_) T();
+    for (size_t i = size_; i > id; i--) {
+        data_[i] = data_[i - 1];
+    }
+    data_[id] = val;
+    size_++;
+    return data_ + id;
+}
+
+template<typename T>
+typename vector<T>::iterator vector<T>::erase(vector::const_iterator pos) {
+    iterator curr = (pos - begin()) + begin();
+    while (curr + 1 != end()) {
+        std::swap(*(curr), *(curr + 1));
+        curr++;
+    }
+    pop_back();
+    return (pos - begin()) + begin();
+}
+
+template<typename T>
+typename vector<T>::iterator vector<T>::erase(vector::const_iterator first, vector::const_iterator last) {
+    size_t from = first - data_;
+    size_t to = last - data_;
+    size_t delta = to - from;
+    for (size_t i = from; i < size_ - delta; i++) {
+        std::swap(data_[i], data_[i + delta]);
+    }
+    for (size_t i = size_ - delta; i < size_; i++) {
+        data_[i].~T();
+    }
+    size_ -= delta;
+    return data_ + to;
+}
+
+
+template<typename T>
+void vector<T>::push_back_realloc(const T &val) {
+    T temp = val;
+    size_t new_capacity = capacity_ == 0 ? 1 : 2 * capacity_;
+    new_buffer(new_capacity);
+    new(data_ + size_) T(temp);
+}
+
+template<typename T>
+void vector<T>::new_buffer(std::size_t new_capacity) {
+    T *new_data = static_cast<T*>(operator new(new_capacity * sizeof(T)));
+    size_t i;
+    try {
+        for (i = 0; i < size_; i++) {
+            new(new_data + i) T(data_[i]);
+        }
+    }
+    catch (...) {
+        while (i != 0) {
+            new_data[i - 1].~T();
+            i--;
+        }
+        operator delete(new_data);
+        throw;
+    }
+    this->~vector();
+    data_ = new_data;
+    capacity_ = new_capacity;
+}
